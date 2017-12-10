63f18dc65bf058c75e88869a66070872698059b5
Ivan Matković <ivan.matkovic@cobeisfresh.com>
2017-12-10 01:14:04 +0100

    Initial Commit

---
+++ User Manager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  User Manager
+//
+//  Created by Ivan Matkovic on 10/12/2017.
+//  Copyright © 2017 Ivan Matkovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
